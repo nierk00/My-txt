2abced053d4d4fa8c53c903dbab0692c3c05f1ee
SGL <105698637+nierk00@users.noreply.github.com>
2025-07-10 19:25:59 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+now = datetime.now()
+print(f"안녕! 지금은 {now.strftime('%Y-%m-%d %H:%M:%S')} 이야! 블로그 봇이 실행되었어!")
+
+print(f"안녕! 지금은 {now.strftime('%Y-%m-%d %H:%M:%S')} 이야! 블로그 봇이 실행되었어!")
